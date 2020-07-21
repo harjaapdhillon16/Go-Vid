f3425543607c9857a400b00a9df864d6fc52c6c5
harjaapdhillon <harvydhillon16@gmail.com>
2020-07-22 00:47:45 +0530

    changes

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://5ac5bbeb0134.ngrok.io",
+  baseURL: "https://00a672082847.ngrok.io",
 });
--- src/components/Profile/OwnPosts.js
+++ src/components/Profile/OwnPosts.js
@@ -1,52 +1,84 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text } from "react-native-paper";
+import { Text, ActivityIndicator } from "react-native-paper";
 import { Video } from "expo-av";
 import { Dimensions } from "react-native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import * as SecureStore from "expo-secure-store";
 
 import Video1 from "../../assets/video1main.mp4";
 import Video2 from "../../assets/video1.mp4";
 import Video3 from "../../assets/video3.mp4";
+
+import theme from "../../utils/theme";
+import firebase from "../../../config";
 
 const { width, height } = Dimensions.get("window");
 
 const Container = styled.View`
   flex-direction: row;
   flex-wrap: wrap;
+  width: 100%;
+  background-color: ${theme.black};
 `;
 
-const VideoFull = styled(Video)`
+const ContainerTop = styled.View`
+  align-items: center;
+  border: 0px solid ${theme.white};
+  border-top-width: 1px;
+`;
+
+const Image = styled.Image`
   width: ${(width * 33) / 100}px;
   height: ${height / 3}px;
+`;
+const BorderView = styled.View`
+  border: 2px solid ${theme.primaryColor};
+  border-top-width:0px;
+  border-left-width: 1px;
+  border-bottom-width:0px;
+  border-right-width: 1px;
 `;
 
 const Data = [Video1, Video2, Video3];
 
-export default function OwnPosts(props) {
+const Loading = () => (
+  <ActivityIndicator
+    color={theme.primaryColor}
+    size={30}
+    style={{ alignSelf: "center", paddingLeft: "45%", paddingTop: 20 }}
+  />
+);
+
+const UserMedia = () => {
+  React.useEffect(() => {
+    async function fetch() {
+      const uid = await SecureStore.getItemAsync("user");
+      await firebase
+        .database()
+        .ref(`posts/${uid}`)
+        .limitToLast(10)
+        .once("value", (snap) => {
+          _setVideoData([...snap.val()]);
+        });
+    }
+    fetch();
+  });
+  const [videoData, _setVideoData] = React.useState([]);
   return (
-    <Container>
-      <VideoFull
-        shouldPlay
-        resizeMode="cover"
-        rate={1.0}
-        isLooping
-        isMuted={true}
-        source={Video1}
-      />
-      <VideoFull
-        rate={1.0}
-        resizeMode="cover"
-        isLooping
-        isMuted={true}
-        source={Video2}
-      />
-      <VideoFull
-        rate={1.0}
-        resizeMode="cover"
-        isLooping
-        isMuted={true}
-        source={Video3}
-      />
-    </Container>
+    <>
+      <Container>
+        {videoData.length === 0 ? (
+          <Loading />
+        ) : (
+          videoData.map((item) => (
+            <BorderView>
+              <Image source={{ uri: item.image }} />
+            </BorderView>
+          ))
+        )}
+      </Container>
+    </>
   );
-}
+};
+export default UserMedia;
